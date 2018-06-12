c5f41535df9e484ebc7ab10df1483a9e9af9ed16
Joerg Roth <info@leylines.ch>
2018-06-12 21:02:30 +0200

    cleanup

--- forbidden_history.sql
+++ forbidden_history.sql
@@ -12,7 +12,7 @@ CREATE ELABEL serves;
 CREATE (n:sovereign_entity {name: 'Holy See', fromdate:'325'});
 CREATE (n:sovereign_entity {name: 'Knights of Malta', other_name: 'Sovereign Military Order of Malta', fromdate: '1099'});
 MATCH  (s:sovereign_entity {name: 'Knights of Malta'}),(d:sovereign_entity {name: 'Holy See'}) CREATE (s)-[:serves {status: 'proven'}]->(d);
-CREATE (n:sovereign_entity {name: 'ICRC', other_name: 'International Red Cross and Red Crescent Movement', fromdate: '1863'});
+-- CREATE (n:sovereign_entity {name: 'ICRC', other_name: 'International Red Cross and Red Crescent Movement', fromdate: '1863'});
 
 -- Continents
 -- Node
@@ -64,7 +64,7 @@ CREATE (n:town {name: 'Athens'});
 MATCH  (s:town {name: 'Athens'}),(d:country {name: 'Greece'}) CREATE (s)-[:located {status: 'proven'}]->(d);
 CREATE (n:town {name: 'Geneva'});
 MATCH  (s:town {name: 'Geneva'}),(d:country {name: 'Switzerland'}) CREATE (s)-[:located {status: 'proven'}]->(d);
-MATCH  (s:sovereign_entity {name: 'ICRC'}),(d:town {name: 'Geneva'}) CREATE (s)-[:located {status: 'proven'}]->(d);
+-- MATCH  (s:sovereign_entity {name: 'ICRC'}),(d:town {name: 'Geneva'}) CREATE (s)-[:located {status: 'proven'}]->(d);
 CREATE (n:town {name: 'Rome'});
 MATCH  (s:town {name: 'Rome'}),(d:country {name: 'Italy'}) CREATE (s)-[:located {status: 'proven'}]->(d);
 MATCH  (s:sovereign_entity {name: 'Holy See'}),(d:town {name: 'Rome'}) CREATE (s)-[:located {status: 'proven'}]->(d);
@@ -125,25 +125,27 @@ MATCH  (s:"order" {name: 'Les Neuf Sœurs'}),(d:"order" {name: 'Freemasonry'}) C
 
 -- Endowments
 -- Node
-CREATE VLABEL endowment;
+-- CREATE VLABEL endowment;
+-- Relations
+-- Data
+-- CREATE (n:endowment {name: 'Jerusalem Islamic Waqf'});
+-- MATCH  (s:endowment {name: 'Jerusalem Islamic Waqf'}),(d:place {name: 'Temple Mountain'}) CREATE (s)-[:controls {status: 'proven'}]->(d);
+
+-- Dynasty
+-- Node
+CREATE VLABEL dynasty;
 -- Relations
 CREATE ELABEL controls;
 -- Data
-CREATE (n:endowment {name: 'Jerusalem Islamic Waqf'});
-MATCH  (s:endowment {name: 'Jerusalem Islamic Waqf'}),(d:place {name: 'Temple Mountain'}) CREATE (s)-[:controls {status: 'proven'}]->(d);
-
--- Families
--- Node
-CREATE VLABEL family;
--- Relations
--- Data
-CREATE (n:family {name: 'Rothschild Dynasty', other_name: 'Guardians of the Papal Treasure', fromdate: '1550'});
-MATCH  (s:family {name: 'Rothschild Dynasty'}),(d:sovereign_entity {name: 'Holy See'}) CREATE (s)-[:controls {fromdate: '1822', status: 'unproven'}]->(d);
-CREATE (n:family {name: 'Merovingian Dynasty'});
-CREATE (n:family {name: 'Pharaos 21. Dynasty'});
-MATCH  (s:family {name: 'Pharaos 21. Dynasty'}),(d:country {name: 'Egypt'}) CREATE (s)-[:located {status: 'proven'}]->(d);
-CREATE (n:family {name: 'Pharaos 22. Dynasty'});
-MATCH  (s:family {name: 'Pharaos 22. Dynasty'}),(d:country {name: 'Egypt'}) CREATE (s)-[:located {status: 'proven'}]->(d);
+CREATE (n:dynasty {name: 'Rothschild Dynasty', other_name: 'Guardians of the Papal Treasure', fromdate: '1550'});
+MATCH  (s:dynasty {name: 'Rothschild Dynasty'}),(d:sovereign_entity {name: 'Holy See'}) CREATE (s)-[:controls {fromdate: '1822', status: 'unproven'}]->(d);
+CREATE (n:dynasty {name: 'Merovingian Dynasty'});
+CREATE (n:dynasty {name: 'Pharaohs'});
+MATCH  (s:dynasty {name: 'Pharaohs'}),(d:country {name: 'Egypt'}) CREATE (s)-[:located {status: 'proven'}]->(d);
+CREATE (n:dynasty {name: '21. Dynasty', fromdate: '1069 BC', todate: '945 BC'});
+MATCH  (s:dynasty {name: '21. Dynasty'}),(d:dynasty {name: 'Pharaohs'}) CREATE (s)-[:located {status: 'proven'}]->(d);
+CREATE (n:dynasty {name: '22. Dynasty', fromdate: '945 BC', todate: '760 BC'});
+MATCH  (s:dynasty {name: '22. Dynasty'}),(d:dynasty {name: 'Pharaohs'}) CREATE (s)-[:located {status: 'proven'}]->(d);
 
 -- Persons
 -- Node
@@ -157,11 +159,11 @@ CREATE ELABEL knows;
 CREATE (n:person {name: 'Benjamin Franklin', fromdate: '1706-01-17', todate: '1790-01-17'});
 MATCH  (s:person {name: 'Benjamin Franklin'}),(d:"order" {name: 'Grand Lodge of Pennsylvania'}) CREATE (s)-[:member_of {status: 'proven', fromdate: '7130'}]->(d);
 MATCH  (s:person {name: 'Benjamin Franklin'}),(d:"order" {name: 'Les Neuf Sœurs'}) CREATE (s)-[:member_of {status: 'proven'}]->(d);
-MATCH  (s:person {name: 'Benjamin Franklin'}),(d:town {name: 'Paris'}) CREATE (s)-[:lives_in {status: 'proven'}]->(d);
-MATCH  (s:person {name: 'Benjamin Franklin'}),(d:country {name: 'USA'}) CREATE (s)-[:lives_in {status: 'proven'}]->(d);
+-- MATCH  (s:person {name: 'Benjamin Franklin'}),(d:town {name: 'Paris'}) CREATE (s)-[:lives_in {status: 'proven'}]->(d);
+-- MATCH  (s:person {name: 'Benjamin Franklin'}),(d:country {name: 'USA'}) CREATE (s)-[:lives_in {status: 'proven'}]->(d);
 CREATE (n:person {name: 'Mary Magdalene'});
 MATCH  (s:person {name: 'Mary Magdalene'}),(d:town {name: 'Saintes-Maries-de-la-Mer'}) CREATE (s)-[:lives_in {fromdate: '45', status: 'unproven'}]->(d);
-MATCH  (s:person {name: 'Mary Magdalene'}),(d:family {name: 'Merovingian Dynasty'}) CREATE (s)-[:belongs_to {status: 'unproven'}]->(d);
+MATCH  (s:person {name: 'Mary Magdalene'}),(d:dynasty {name: 'Merovingian Dynasty'}) CREATE (s)-[:belongs_to {status: 'unproven'}]->(d);
 CREATE (n:person {name: 'Plato', fromdate: '428 BC', todate: '348 BC'});
 MATCH  (s:person {name: 'Plato'}),(d:town {name: 'Athens'}) CREATE (s)-[:lives_in {status: 'proven'}]->(d);
 MATCH  (s:person {name: 'Plato'}),(d:country {name: 'Egypt'}) CREATE (s)-[:lives_in {status: 'proven'}]->(d);
