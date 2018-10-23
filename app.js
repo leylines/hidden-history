e3fc797a18531df2621bc10bac1603c54b438878
Joerg Roth <info@leylines.ch>
2018-10-23 19:53:18 +0200

    add CORS and new leaflet-layer

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(session({
     proxy: true,
     cookie: { secure: true }
 })); // session secret
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash());
